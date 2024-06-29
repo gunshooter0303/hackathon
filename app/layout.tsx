6cf8d0d5852ba5ef41b9f9327393c991126f4585
Sai Kaushik <saikaushikchintala.gmail.com>
2024-06-29 13:40:20 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Recursive } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import About from "@/components/About";
 
 const inter = Inter({ subsets: ["latin"] });
+const recursive = Recursive({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +21,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn("", recursive.className, inter.className)}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          themes={["dark", "light"]}
+        >
+          <Navbar />
+          {children}
+          {/* <About /> */}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
